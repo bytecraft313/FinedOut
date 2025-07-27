de64b37ae31093714126c3ca29e7a35e7d84edc1
bytecraft313 <cyber313ace@proton.me>
2025-07-27 14:46:23 +0430

    add remove button and functionality to removea friend from the list

--- app/src/main/java/com/example/finedout/MainActivity.kt
+++ app/src/main/java/com/example/finedout/MainActivity.kt
@@ -4,19 +4,40 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.finedout.ui.theme.FinedOutTheme
-import com.example.finedout.util.PreferencesHelper
-import androidx.compose.ui.platform.LocalContext
 import com.example.finedout.util.DateUtils
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+import com.example.finedout.util.PreferencesHelper
 
 
 class MainActivity : ComponentActivity() {
@@ -119,8 +140,22 @@ fun FineTrackerApp(context: Context = LocalContext.current) {
                         ) {
                             Text("+")
                         }
+
+                        Spacer(modifier = Modifier.width(4.dp))
+
+                        IconButton(onClick = {
+                            peopleList = peopleList.toMutableList().also {
+                                it.removeAt(index)
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Remove Friend"
+                            )
+                        }
                     }
                 }
+
             }
         }
 
